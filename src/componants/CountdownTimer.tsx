feb3e462749134f019c2f370553e17d7945a8830
kapilwedsvaishnavi <kapilwedsvaishnavi@gmail.com>
2026-02-10 18:18:17 +0530

    minor error fix

--- src/componants/CountdownTimer.tsx
+++ src/componants/CountdownTimer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import Countdown from "react-countdown";
 import TimerFrame from "../ui/TimerFrame";
 import { motion, useInView } from "framer-motion";
